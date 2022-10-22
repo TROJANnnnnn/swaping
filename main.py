6e62a6670c9a95461c4faefa50dcccd9b059ce97
codinggandhi <116358825+codinggandhi@users.noreply.github.com>
2022-10-22 09:49:14 +0530

    Updated

--- main.py
+++ main.py
@@ -200,9 +200,9 @@ Q = int(input("Please enter value for Q: "))
 
 # To swap the value of two variables
 # we will user third variable which is a temporary variable
-temp_1 = P
+temp = P
 P = Q
-Q = temp_1
+Q = temp
 
 print("The Value of P after swapping: ", P)
 print("The Value of Q after swapping: ", Q
